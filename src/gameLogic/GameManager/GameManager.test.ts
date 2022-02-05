e486290e3d9d558325a3fe97eabbe67e5fa9a8bf
ValentineSokol <valentinesokolovskiy@gmail.com>
2022-02-05 22:27:05 +0200

    added a mcok test to verify the CD pipeline

--- src/gameLogic/GameManager/GameManager.test.ts
+++ src/gameLogic/GameManager/GameManager.test.ts
@@ -1,4 +1,8 @@
 // eslint-disable-next-line no-undef
 describe('Checkers Game Manager', () => {
-
+  // eslint-disable-next-line no-undef
+  it('test to pass CI', () => {
+    // eslint-disable-next-line no-undef
+    expect(2 + 2).toBe(4);
+  });
 });
