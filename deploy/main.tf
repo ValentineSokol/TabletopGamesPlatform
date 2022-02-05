02893bbfbdce2a15e6606aecade1fcdef5426fad
ValentineSokol <valentinesokolovskiy@gmail.com>
2022-02-05 12:49:01 +0200

    added eslint, terraform and CD

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,11 +1,23 @@
 
    provider "aws" {
       region = "eu-west-2"
-      access_key = "AKIA525FNXQXF772QIWX"
-      secret_key = "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"
     }
 
-   resource "aws_s3_bucket" "tabletop_games" {
-     ami = "ami-0015a39e4b7c0966f"
-     instance_type = "t3.micro"
+   terraform {
+     backend "s3" {
+       bucket = "tabletop-terraform-backend"
+       key = "terraform"
+       region = "eu-west-2"
+       dynamodb_table = "tabletop-terraform-lock"
+     }
+   }
+
+   resource "aws_s3_bucket" "tabletop-games-client" {
+     bucket = "tabletop-games-client"
+     acl = "public-read"
+     policy = file("bucketPolicy.json")
+     website {
+       index_document = "index.html"
+       error_document = "index.html"
+     }
    }
